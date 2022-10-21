2c6fb298ded896e58b91f128f8c45f73ba88fc43
NettoJM <nettojm86@gmail.com>
2022-10-21 19:50:44 -0300

    PlanetCard component rendered inside SolarSystem component

--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
+import planets from '../data/planets';
+import PlanetCard from './PlanetCard';
 import Title from './Title';
 
 class SolarSystem extends Component {
   render() {
+    const solarSystem = planets.map(({ name, image }) => (
+      <PlanetCard key={ name } planetName={ name } planetImage={ image } />
+    ));
     return (
       <div data-testid="solar-system">
         <Title headline="planetas" />
+        { solarSystem }
       </div>
     );
   }
