8052eaa4b643c7b2e369406d8e90c4c9b17903c0
NettoJM <nettojm86@gmail.com>
2022-10-21 20:12:25 -0300

    MissionCard component rendered inside Missions component

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
+import missions from '../data/missions';
+import MissionCard from './MissionCard';
 import Title from './Title';
 
 class Missions extends Component {
   render() {
+    const missionCards = missions.map(
+      ({ name, year, country, destination }) => (
+        <MissionCard
+          key={ name }
+          name={ name }
+          year={ year }
+          country={ country }
+          destination={ destination }
+        />
+      ),
+    );
     return (
       <div data-testid="missions">
         <Title headline="Missões" />
+        { missionCards }
       </div>
     );
   }
