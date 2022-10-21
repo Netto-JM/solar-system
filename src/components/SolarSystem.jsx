1fbdc1c11153e1dc5fd22cd528f2b5898ac270ce
NettoJM <nettojm86@gmail.com>
2022-10-21 17:57:47 -0300

    SolarSystem component added

---
+++ src/components/SolarSystem.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SolarSystem extends Component {
+  render() {
+    return (
+      <div data-testid="solar-system">
+        Content
+      </div>
+    );
+  }
+}
+
+export default SolarSystem;
